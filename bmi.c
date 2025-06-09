6f252552894aebf5cfe1a833f715fc388220d443
Eri Fröstl <erifrostl06@gmail.com>
2025-06-08 23:31:12 -0300

    Update bmi.c
    Se agrega un prinf que dice la condición según el indice

--- bmi.c
+++ bmi.c
@@ -13,7 +13,19 @@ float peso, altura;
     
 float index=peso/(altura*altura);
 
-    printf("Su índice de masa corporal es: %.2f\n\nÍndice | Condición\n-----------------------------\n<18.5 | Bajo peso\n18.5 a 24.9 | Normal\n25.0 a 29.9 | Sobrepeso\n>=30| Obesidad",index);
+    printf("Su índice de masa corporal es: %.2f\n\nÍndice | Condición\n-----------------------------\n<18.5 | Bajo peso\n18.5 a 24.9 | Normal\n25.0 a 29.9 | Sobrepeso\n>=30| Obesidad\n\n",index);
 
-    return 0;
+if(index<18.5)
+printf("Condición: Bajo peso");
+else{
+    if(index<25)
+    printf("Condición: Normal");
+    else{
+        if(index<30)
+        printf("Condición: Sobrepeso");
+        else{
+            if(index>=30)
+            printf("Condición: Obesidad");
+}}}
+return 0;
 }
